f31d70d6b92ec830ef4bab5534bdf1e25f931cd4
C1 <c1@gjun.com>
2017-12-29 13:54:40 +0800

    Initial commit

---
+++ app/src/main/java/com/wilson/a2017122703/MainActivity.java
@@ -0,0 +1,38 @@
+package com.wilson.a2017122703;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.EditText;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
+    EditText ed1;
+    EditText ed2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv = (TextView) findViewById(R.id.textView4);
+        ed1 = (EditText) findViewById(R.id.editText4);
+        ed2 = (EditText) findViewById(R.id.editText5);
+
+        tv.setText("Hello World");
+    }
+    public void click1(View v)
+    {
+        String str = ed1.getText().toString()+"+"+ed2.getText().toString()+"=";
+
+        //tv.setText(ed1.getText().toString()+ed2.getText().toString());
+
+        //String sum = parseInt(ed1.getText().toString())+parseInt(ed2.getText().toString());
+        int sum = Integer.parseInt(ed1.getText().toString())+Integer.valueOf(ed2.getText().toString());
+        tv.setText(String.valueOf(sum));
+        //tv.setText(sum);
+    }
+}
